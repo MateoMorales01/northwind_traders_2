b3a0a699e43f7891403300f14b6601a39a691cbf
Mateo Morales <Moralmat76@gmail.com>
2025-12-03 10:30:57 -0600

    added notes

---
+++ src/main/java/com/northwind/Program.java
@@ -0,0 +1,11 @@
+package com.northwind;
+
+public class Program {
+    public static void main(String[] args) {
+        String username = args [0];
+        String password = args [1];
+
+        System.out.println(username);
+        System.out.println(password);
+    }
+}
